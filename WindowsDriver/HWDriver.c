183bd07d8c2a7a0537a71d4c3df5e40c349e6da6
Rob Beasley <rob@beasleyville.com>
2025-04-23 07:16:58 -0700

    HellowWorld windows driver from MS tutorial.

---
+++ WindowsDriver/HWDriver.c
@@ -0,0 +1,53 @@
+#include <ntddk.h>
+#include <wdf.h>
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+
+NTSTATUS
+DriverEntry( 
+    _In_ PDRIVER_OBJECT     DriverObject,
+    _In_ PUNICODE_STRING    RegistryPath
+)
+{
+
+    NTSTATUS status = STATUS_SUCCESS;
+
+    WDF_DRIVER_CONFIG config;
+
+    KdPrintEx(( DPFLTR_IHVDRIVER_ID, DPFLTR_TRACE_LEVEL, "KmdfHelloWorld: DriverEntry\n" ));
+
+    WDF_DRIVER_CONFIG_INIT( &config, KmdfHelloWorldEvtDeviceAdd );
+
+    status = WdfDriverCreate( DriverObject,
+                              RegistryPath,
+                              WDF_NO_OBJECT_ATTRIBUTES,
+                              &config,
+                              WDF_NO_HANDLE
+                            );
+    return status;
+}
+
+NTSTATUS
+KmdfHelloWorldEvtDeviceAdd(
+    _In_ WDFDRIVER          Driver,
+    _Inout_ PWDFDEVICE_INIT DeviceInit
+)
+{
+    // prevent warnings
+    UNREFERENCED_PARAMETER( Driver );
+
+    NTSTATUS status;
+
+    // handle to the device
+    WDFDEVICE hDevice;
+
+    KdPrintEx(( DPFLTR_IHVDRIVER_ID, DPFLTR_TRACE_LEVEL, "KmdfHelloWorld: Driver Device Add\n" ));
+
+    status = WdfDeviceCreate( &DeviceInit,
+                              WDF_NO_OBJECT_ATTRIBUTES,
+                              &hDevice
+                            );
+    return status;
+}
+
